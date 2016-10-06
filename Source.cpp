8b7a8ff6cbc5376692195c5003cfa658abb40a27
jrobinson-mcclure19 <jrobinson-mcclure19@my.whitworth.edu>
2016-10-05 20:25:34 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+///////////////////
+// Jalen Robinson-McClure
+// October 5th 
+// CS 172
+// PRACTICE EXAM
+///////////////////////
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+}
